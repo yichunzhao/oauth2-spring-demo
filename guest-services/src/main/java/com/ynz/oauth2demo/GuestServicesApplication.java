efa8f070cbbdc18417bba2d684c557fa2e3ef2ce
yichunzhao <zhaoyichun@hotmail.com>
2020-08-26 04:46:50 +0200

    missed files

---
+++ guest-services/src/main/java/com/ynz/oauth2demo/GuestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.oauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestServicesApplication.class, args);
+	}
+
+}
---
+++ guest-services/src/test/java/com/ynz/oauth2demo/GuestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.oauth2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
