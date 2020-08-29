805efec5d6fd24539df5099dc89c4f414d93892d
yichunzhao <zhaoyichun@hotmail.com>
2020-08-29 19:46:49 +0200

    new module for guest-app

---
+++ guest-app/src/main/java/com/ynz/oauth2demo/GuestAppApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.oauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestAppApplication.class, args);
+	}
+
+}
